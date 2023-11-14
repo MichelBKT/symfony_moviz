521b0b33f00c199902353976f004034c873737d2
Michel Banckaert <banckaertm@hotmail.com>
2023-11-14 22:26:06 +0100

    session 5

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MovieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends AbstractController
+{
+    #[Route('/movie', name: 'app_movie')]
+    public function index(MovieRepository $movieRepository): Response
+    {
+        $movies = $movieRepository->findBy([], ['id' => 'DESC']);
+
+        return $this->render('movie/index.html.twig', [
+            'movies' => $movies,
+        ]);
+    }
+}
